6716b39387680ccd4565c566ba81f485bab120d8
jagdishbutte <jagdishbutte@gmail.com>
2026-08-09 14:39:19 +0530

    fix: jenkinsfile moved to root

--- ai-service/app/database/mongo.py
+++ ai-service/app/database/mongo.py
@@ -1,7 +1,7 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import AsyncMongoClient
 from app.config import settings
 
-client = AsyncIOMotorClient(settings.MONGO_URI)
+client = AsyncMongoClient(settings.MONGO_URI)
 database = client[settings.MONGO_DATABASE]
 
 test_logs = database["test_logs"]
